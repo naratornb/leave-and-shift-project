5e5959f0fdf2e5aba00ede515cdcab80da8ecdf7
naratorn.b <naratorn.b@kasikornline.com>
2025-08-12 14:27:06 +1000

    LSM-87 feat: add form & context for employee creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,16 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import ShiftForm from "./components/ShiftForm";
 import ShiftList from "./components/ShiftList";
+import EmployeeForm from "./components/EmployeeForm";
+import EmployeeList from "./components/EmployeeList";
 import {AuthProvider} from "./context/AuthContext";
 import {ShiftProvider} from "./context/ShiftContext";
+import {EmployeeProvider} from "./context/EmployeeContext";
 
 function App() {
   return (
     <AuthProvider>
+      <EmployeeProvider>
       <ShiftProvider>
         <Router>
           <div className="min-h-screen bg-gray-100">
@@ -22,15 +26,20 @@ function App() {
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/profile" element={<Profile/>}/>
-                {/* Other routes */}
+                {/* Shift Management routes */}
                 <Route path="/shifts" element={<ShiftList/>}/>
                 <Route path="/shifts/new" element={<ShiftForm/>}/>
                 <Route path="/shifts/:id/edit" element={<ShiftForm/>}/>
+                {/* Employee Management routes */}
+                <Route path="/employees" element={<EmployeeList />} />
+                <Route path="/employees/new" element={<EmployeeForm />} />
+                <Route path="/employees/:id/edit" element={<EmployeeForm />} />
               </Routes>
             </main>
           </div>
         </Router>
       </ShiftProvider>
+      </EmployeeProvider>
     </AuthProvider>
   );
 }
---
+++ frontend/src/pages/Employees.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const API_URL = '/api/employees/';
+
+// Create new employee
+const createEmployee = async (employeeData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+
+  const response = await axios.post(API_URL, employeeData, config);
+  return response.data;
+};
+
+const employeeService = {
+  createEmployee,
+  getEmployees,
+  getEmployee,
+  updateEmployee,
+  deleteEmployee,
+  deactivateEmployee,
+  activateEmployee
+};
+
+export default employeeService;
