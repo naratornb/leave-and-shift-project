09225d95d1b8b6c5267b7b283a3e6911a850f385
naratorn.b <naratorn.b@kasikornline.com>
2025-08-13 00:09:43 +1000

    LSM-60 fix: fix error unused variable

--- frontend/src/components/ShiftForm.jsx
+++ frontend/src/components/ShiftForm.jsx
@@ -5,7 +5,6 @@ import {useAuth} from '../context/AuthContext';
 
 const ShiftForm = () => {
     const {createShift, updateShift, getShift, shift, isLoading, error, success, resetSuccess} = useShiftContext();
-    const {user} = useAuth();
     const navigate = useNavigate();
     const {id} = useParams();
     const isEdit = Boolean(id);
