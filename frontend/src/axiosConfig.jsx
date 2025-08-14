90cf16c388c2b50bab6277e8a676214947809360
naratorn.b <naratorn.b@kasikornline.com>
2025-08-14 14:33:37 +1000

    fix: revert axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = process.env.ENV_NAME === 'production'? 'http://54.66.213.198:5001' : 'http://localhost:5001';
-console.log("axiosconfigIP:" + baseUrl);
 const axiosInstance = axios.create({
-  baseURL: baseUrl,
+  // baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://54.66.213.198:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
