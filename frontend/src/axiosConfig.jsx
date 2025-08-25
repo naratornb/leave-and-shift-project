d403d64829543fd0a7c96a7df716b9f380ced9d0
naratorn.b <naratorn.b@kasikornline.com>
2025-08-25 16:07:19 +1000

    test change

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
+  // change
   // baseURL: 'http://localhost:5001', // local
   baseURL: 'http://54.66.213.198:5001', // live
   headers: { 'Content-Type': 'application/json' },
